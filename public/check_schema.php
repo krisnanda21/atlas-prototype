9912c7a8eaf9b66284eda925652bc855c3a8c27b
Krisnanda Prasetya <krisnanda@atlas.local>
2026-08-24 19:10:54 +0700

    chore: remove unused and risky one-off scripts from public directory

--- public/check_schema.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle($request = Illuminate\Http\Request::capture());
-use Illuminate\Support\Facades\DB;
-
-$res = DB::select('SHOW CREATE TABLE competency_gaps');
-print_r($res);
--- public/check_simpel_dates.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle($request = Illuminate\Http\Request::capture());
-use Illuminate\Support\Facades\DB;
-
-$res = DB::table('simpel_t_diklat')->select('kode_pelatihan', 'nama_pelatihan', 'tanggal_mulai', 'tanggal_selesai')->limit(5)->get();
-print_r($res);
